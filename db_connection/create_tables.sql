a5b4108399dddf799ef5deaba4de0ecdddf56c77
char-lie <webmaster976@gmail.com>
2015-02-23 00:01:56 +0200

    Created SQL-file for DB tables creation

---
+++ db_connection/create_tables.sql
@@ -0,0 +1,150 @@
+-- Parse::SQL::Dia          version 0.27                              
+-- Documentation            http://search.cpan.org/dist/Parse-Dia-SQL/
+-- Environment              Perl 5.018002, /usr/bin/perl              
+-- Architecture             x86_64-linux-gnu-thread-multi             
+-- Target Database          mysql-myisam                              
+-- Input file               w.dia                                     
+-- Generated at             Sun Feb 22 23:52:52 2015                  
+-- Typemap for mysql-myisam not found in input file                   
+
+-- get_constraints_drop 
+
+-- get_permissions_drop 
+
+-- get_view_drop
+
+-- get_schema_drop
+drop table if exists user;
+drop table if exists lnk_user_place;
+drop table if exists place;
+drop table if exists city;
+drop table if exists country;
+drop table if exists photo_album;
+drop table if exists photo;
+drop table if exists gender;
+drop table if exists preference;
+drop table if exists appearance;
+drop table if exists physique;
+drop table if exists eyes_color;
+drop table if exists hair_color;
+drop table if exists hair_length;
+drop table if exists personal;
+drop table if exists lnk_user_interest;
+drop table if exists interest;
+
+-- get_smallpackage_pre_sql 
+
+-- get_schema_create
+create table user (
+   id_user                  not null,
+   email      varchar2(256) not null,
+   password   varchar2(100) not null,
+   id_gender  int                   ,
+   birth_date date                  ,
+   constraint pk_user primary key (id_user)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table lnk_user_place (
+   id_lnk_user_place int not null,
+   id_user           int not null,
+   id_place          int not null,
+   constraint pk_lnk_user_place primary key (id_lnk_user_place)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table place (
+   id_place    int           not null,
+   id_city                   not null,
+   name        varchar2(200) not null,
+   description varchar2(500)         ,
+   constraint pk_place primary key (id_place)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table city (
+   id_city    int          not null,
+   id_country int          not null,
+   name       varchar(200) not null,
+   constraint pk_city primary key (id_city)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table country (
+   id_country int           not null,
+   name       varchar2(200) not null,
+   constraint pk_country primary key (id_country)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table photo_album (
+   id_photo_album int           not null,
+   id_user        int                   ,
+   name           varchar2(200) not null,
+   description    varchar2(500)         ,
+   id_main_photo  int                   ,
+   constraint pk_photo_album primary key (id_photo_album)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table photo (
+   id_photo    int           not null,
+   id_album    int                   ,
+   name        varchar2(200)         ,
+   description varchar2(500)         
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table gender (
+   id_gender int          not null,
+   name      varchar2(50) not null,
+   constraint pk_gender primary key (id_gender)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table preference (
+   id_preference int not null,
+   id_user       int not null,
+   id_gender     int         ,
+   constraint pk_preference primary key (id_preference)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table appearance (
+   id_user        int not null,
+   height         int         ,
+   id_physique    int         ,
+   id_eyes_color  int         ,
+   id_hair_color  int         ,
+   id_hair_length int         ,
+   constraint pk_appearance primary key (id_user)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table physique (
+   id_physique int          not null,
+   name        varchar2(50) not null,
+   constraint pk_physique primary key (id_physique)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table eyes_color (
+   id_eyes_color int          not null,
+   name          varchar2(50) not null,
+   constraint pk_eyes_color primary key (id_eyes_color)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table hair_color (
+   id_hair_color int          not null,
+   name          varchar2(50) not null,
+   constraint pk_hair_color primary key (id_hair_color)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table hair_length (
+   id_hair_length int          not null,
+   name           varchar2(50) not null,
+   constraint pk_hair_length primary key (id_hair_length)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table personal (
+   id_user         not null,
+   id_gender  int          ,
+   birth_date date         ,
+   constraint pk_personal primary key (id_user)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table lnk_user_interest (
+   id_lnk_user_interest int not null,
+   id_user              int not null,
+   id_interest          int not null,
+   constraint pk_lnk_user_interest primary key (id_lnk_user_interest)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table interest (
+   id_interest int           not null,
+   name        varchar2(200)         ,
+   constraint pk_interest primary key (id_interest)
+)   ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- get_view_create
+
+-- get_permissions_create
+
+-- get_inserts
+
+-- get_smallpackage_post_sql
+
+-- get_associations_create
